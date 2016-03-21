3ad203c239bfee160720b957e86c5a66c2cd0eac
James Hay <james@90seconds.tv>
2016-03-21 16:36:27 +0800

    god

--- listeria/client/components/content/add_node.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'; // eslint-disable-line no-unused-vars
-
-const AddNode = ({ onAdd }) => (
-  <div className="add_node">
-    <button onClick={onAdd}>Add New Node</button>
-  </div>
-);
-
-export { AddNode };
